49731fcce9a18c97cf2924ae67e3983a069459f6
David-dev8 <72741163+David-dev8@users.noreply.github.com>
2023-03-06 11:22:55 +0100

    Initial class declerations
    Co-Authored-By: Nathan511 <72741392+Nathan511@users.noreply.github.com>
    Co-Authored-By: Tim <72740253+Tim070@users.noreply.github.com>
    Co-Authored-By: Talip <73100121+talip-stenden@users.noreply.github.com>

---
+++ Eindopdracht/Lifethreadening/DataAccess/Database/DatabaseSimulationWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lifethreadening.DataAccess.Database
+{
+    public class DatabaseSimulationWriter: ISimulationWriter
+    {
+    }
+}
