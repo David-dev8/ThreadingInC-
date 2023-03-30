277b265da4a0403ab5076bca47513410c85da38c
Nathan511 <72741392+Nathan511@users.noreply.github.com>
2023-03-30 11:17:59 +0200

    Some changes for the simulation reader

--- Eindopdracht/Lifethreadening/DataAccess/Database/DatabaseSimulationReader.cs
+++ Eindopdracht/Lifethreadening/DataAccess/Database/DatabaseSimulationReader.cs
@@ -1,6 +1,8 @@
-﻿using Lifethreadening.Models;
+﻿using Lifethreadening.ExtensionMethods;
+using Lifethreadening.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -17,22 +19,39 @@ namespace Lifethreadening.DataAccess.Database
 
         public IEnumerable<Simulation> ReadAll()
         {
-            throw new NotImplementedException();
+            DatabaseHelper<Simulation> database = new DatabaseHelper<Simulation>();
+            string query = @"
+                SELECT S.*, E.id AS ecosystemId, E.name AS ecosystemName, E.image, E.difficulty
+                FROM Simulation AS S
+                JOIN Ecosystem AS E ON E.id = S.ecosystemId";
+
+            IEnumerable<SqlParameter> parameters = new List<SqlParameter>
+            {
+                //new SqlParameter("@ecosystemId", ecosystemId),
+            };
+            return database.Read(CreateSimulation, query, CommandType.Text, parameters);
         }
 
-        //private Simulation CreateSimulation(SqlDataReader dataReader)
-        //{
-            //return new Simulation(
-            //    dataReader.GetInt32("id"),
-            //    dataReader.GetString("name"),
-            //    dataReader.GetString("description"),
-            //    dataReader.GetString("scientificName"),
-            //    dataReader.GetString("image"),
-            //    dataReader.GetInt32("averageAge"),
-            //    dataReader.GetInt32("maxAge"),
-            //    dataReader.GetInt32("maxBreedSize"),
-            //    dataReader.GetInt32("minBreedSize"),
-            //);
-        //}
+        private Simulation CreateSimulation(SqlDataReader dataReader)
+        {
+            return new Simulation(
+                dataReader.GetInt32("id"),
+                dataReader.GetInt32("score"),
+                dataReader.GetDateTime("dateStarted"),
+                dataReader.GetDateTime("dateEnded"),
+                dataReader.GetInt32("amountOfDisasters"),
+                dataReader.GetString("name"),
+                dataReader.GetString("fileNameSaveSlot"), // TODO saveslot
+                new Ecosystem(
+                    dataReader.GetInt32("id"),
+                    dataReader.GetString("name"),
+                    dataReader.GetString("image"),
+                    dataReader.GetFloat("difficulty")
+                )
+    
+            );
+        }
+
+        private Sim
     }
 }
