faba9d608e0b31da6ddfd121e2b6e536fe29b780
Tim <72740253+Tim070@users.noreply.github.com>
2023-03-31 09:38:31 +0200

    Extension comments

--- Eindopdracht/Lifethreadening/ExtensionMethods/RandomExtensions.cs
+++ Eindopdracht/Lifethreadening/ExtensionMethods/RandomExtensions.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace Lifethreadening.ExtensionMethods
 {
+    /// <summary>
+    /// This class contains a set of extension methods for objects of type Random 
+    /// </summary>
     public static class RandomExtensions
     {
+        /// <summary>
+        /// This method generates a random Double value
+        /// </summary>
+        /// <param name="random">The random object to use</param>
+        /// <param name="min">The lowest inclusive value the random value may have</param>
+        /// <param name="max">The highest exclusive value the random value may have</param>
+        /// <returns>A random double value between the 2 given values</returns>
         public static double NextDouble(this Random random, double min = 0, double max = 1)
         {
             return random.NextDouble() * (max - min) + min;
