6e9621b3f6f8f45270ee940c466f375d932342f1
Tim <72740253+Tim070@users.noreply.github.com>
2023-03-31 09:40:53 +0200

    Helpers comments

--- Eindopdracht/Lifethreadening/Helpers/EnumHelpers.cs
+++ Eindopdracht/Lifethreadening/Helpers/EnumHelpers.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 
 namespace Lifethreadening.Helpers
 {
+    /// <summary>
+    /// This class contains a set of extension methods for Enumerable objects
+    /// </summary>
     public class EnumHelpers
     {
         private static Random _random = new Random();
 
+        /// <summary>
+        /// Returns a random ENUM value from the posible values
+        /// </summary>
+        /// <typeparam name="T">The ENUM type to get a random value for</typeparam>
+        /// <returns>A random value for the given ENUM type</returns>
         public static T GetRandom<T>() where T : Enum
         {
             Array values = Enum.GetValues(typeof(T));
